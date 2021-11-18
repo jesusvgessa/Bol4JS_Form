c49c83ce0de7a8594e675dc4d8133e9cdcc03b2f
jesusvgessa <jesusvgessa@hotmail.com>
2021-11-18 11:42:50 +0100

    change

---
+++ Actividades JS/script.js
@@ -0,0 +1,6 @@
+var nombre = window.document.getElementById('nombre').addEventListener('onblur', obNombre, true);
+
+function obNombre() {
+    var nombre = document.getElementById("nombre");
+    x.value = x.value.toUpperCase();
+}
