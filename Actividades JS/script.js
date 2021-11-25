486bbc3fde67ca1dd59276bac74303aaff4c1a67
jesusvgessa <jesusvgessa@hotmail.com>
2021-11-25 12:36:54 +0100

    commit

--- Actividades JS/script.js
+++ Actividades JS/script.js
@@ -3,7 +3,6 @@ window.onload = load;
 function load() {
     //Entorno:
     var formulario = document.getElementById('formulario');
-    var enviar = false;
 
     //Inputs
     var nombre = document.getElementById("nombre");
@@ -42,15 +41,31 @@ function load() {
     var patternEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
     // ^ Busca el principio de nuestra cadena.
-    // ? Permite que haya 0 o 1 vez nuestra expresión anterior (.-)
-    // | le indico que uno u otro, y en este caso tengo 3 opciones
-    // 
+    // Despues 2 digitos
+    // Le doy la opcion de utilizar / o -
+    // Continuo con otros 2 digitos
+    // Repito dandole la opcion de / o -
+    // Termino con 4 digitos
     var patternFecha = /^(\d{2})(\/|\-)\d{2}(\/|\-)\d{4}$/;
 
     // Le indico el rango de valores, y el numero de veces que tiene que aparecer
     var patternTelefono = /\d{9}/;
 
+    //Empiezo el pattern con 2 digitos
+    //continuo con un caracter :
+    //Termino nuevamente con 2 digitos
     var patternHora = /^(\d{2})\:(\d{2})$/;
+
+    //Asigno un interruptor a cada variable para despues realizar la validación del formulario
+    var enviarNombre = false;
+    var enviarApellido = false;
+    var enviarEdad = false;
+    var enviarNif = false;
+    var enviarEmail = false;
+    var enviarProvincia = false;
+    var enviarFecha = false;
+    var enviarTef = false;
+    var enviarHora = false;
 
     //Eventos:
     nombre.addEventListener("focusout", ponerMayus);
@@ -64,9 +79,18 @@ function load() {
     fecha.addEventListener("focusout", validarFecha);
     telefono.addEventListener("focusout", validarTelefono);
     hora.addEventListener("focusout", validarHora);
-    formulario.addEventListener('submit', validarEnviar);
+    formulario.addEventListener("submit", validarEnviar);
 
     //Algoritmo:
+    //Hago una funcion que voy a repetir en varias partes del codigo
+    //La uso para borrar el error cuando voy a realizar una validacion, y que 
+    //los errores salgan de uno en uno.
+    function borrarError(){
+        if (error.innerText.length != 0){
+            error.removeChild(error.firstChild);
+        }//Fin Si
+    }//Fin Función
+
     //Ejercicio 2
     function ponerMayus() {
         this.value = this.value.toUpperCase();
@@ -74,154 +98,149 @@ function load() {
 
     //Ejercicio 3
     function validar() {
-        if (error.innerText.length != 0){
-            error.removeChild(error.firstChild);
-        }//Fin Si
+        borrarError();
         if (this.value == null || this.value == ""){
             error.innerText = "ERROR: "+this.id.toUpperCase()+" no se puede quedar vacio.";
             this.focus();
             this.setAttribute("class","error");
-            enviar = false;
+            if (this.id == "nombre"){
+                enviarNombre = false;
+            }else{
+                enviarApellido = false;
+            }//Fin Si
         }else if(!patternNomApe.test(this.value)){
             error.innerText = "ERROR: "+this.id.toUpperCase()+" debe contener solo letras.";
             this.focus();
             this.setAttribute("class","error");
-            enviar = false;
+            if (this.id == "nombre"){
+                enviarNombre = false;
+            }else{
+                enviarApellido = false;
+            }//Fin Si
         }else{
             this.removeAttribute("class");
-            enviar = true;
+            if (this.id == "nombre"){
+                enviarNombre = true;
+            }else{
+                enviarApellido = true;
+            }//Fin Si
         }//Fin Si
     }//Fin Funcion
 
     //Ejercicio 4
     function validarEdad() {
-        if (error.innerText.length != 0){
-            error.removeChild(error.firstChild);
-        }//Fin Si
+        borrarError();
         if (this.value == "" || this.value == null){
             error.innerHTML ="ERROR: "+this.id.toUpperCase()+" no se puede quedar vacio.";
             this.setAttribute("class","error");
             this.focus();
-            enviar = false;
+            enviarEdad = false;
         } else if (isNaN(this.value)){
             error.innerHTML ="ERROR: "+this.id.toUpperCase()+" debe ser un numero.";
             this.setAttribute("class","error");
             this.focus();
-            enviar = false;
+            enviarEdad = false;
         } else if (this.value < 0 || this.value > 105) {
             error.innerHTML ="ERROR: "+this.id.toUpperCase()+" debe estar en el rango de 0-105.";
             this.setAttribute("class","error");
             this.focus();
-            enviar = false;
+            enviarEdad = false;
         }else{
             this.removeAttribute("class");    
-            enviar = true;
+            enviarEdad = true;
         }//Fin Si
     }//Fin Funcion
 
     //Ejercicio 5
     function validarNif() {
-        if (error.innerText.length != 0){
-            error.removeChild(error.firstChild);
-        }//Fin Si
+        borrarError();
         if (!patternNif.test(this.value)){
             error.innerHTML ="ERROR: "+this.id.toUpperCase()+" debe contener 8 numero seguido de un guion y letra ( 88888888-A ).";
             this.setAttribute("class","error");
             this.focus();
-            enviar = false;
+            enviarNif = false;
         }else{
             this.removeAttribute("class");
-            enviar = true;
+            enviarNif = true;
         }//Fin Si
     }//Fin Funcion
 
     //Ejercicio 6
     function validarEmail() {
-        if (error.innerText.length != 0){
-            error.removeChild(error.firstChild);
-        }//Fin Si
+        borrarError();
         if (!patternEmail.test(this.value)){
             error.innerHTML ="ERROR: "+this.id.toUpperCase()+" debe contener @ y .";
             this.setAttribute("class","error");
             this.focus();
-            enviar = false;
+            enviarEmail = false;
         }else{
             this.removeAttribute("class");
-            enviar = true;
+            enviarEmail = true;
         }//Fin Si
     }//Fin Funcion
 
     //Ejercicio 7
     function validarProvincia() {
-        if (error.innerText.length != 0){
-            error.removeChild(error.firstChild);
-        }//Fin Si
+        borrarError();
         if (this.value == 0){
             error.innerHTML ="ERROR: "+this.id.toUpperCase()+" debe seleccionar una opcion";
             this.setAttribute("class","error");
             this.focus();
-            enviar = false;
+            enviarProvincia = false;
         }else{
             this.removeAttribute("class");
-            enviar = true;
+            enviarProvincia = true;
         }//Fin Si
     }//Fin Funcion
 
     //Ejercicio 8
     function validarFecha() {
-        if (error.innerText.length != 0){
-            error.removeChild(error.firstChild);
-        }//Fin Si
+        borrarError();
         if (!patternFecha.test(this.value)){
             error.innerHTML = this.id[0].toUpperCase() + this.id.slice(1)+" errónea. Introdúzcala de nuevo. (dd/mm/yyyy)";
             this.setAttribute("class","error");
             this.focus();
-            enviar = false;
+            enviarFecha = false;
         }else{
             this.removeAttribute("class");
-            enviar = true;
+            enviarFecha = true;
         }//Fin Si
     }//Fin Funcion
 
     //Ejercicio 9
     function validarTelefono() {
-        if (error.innerText.length != 0){
-            error.removeChild(error.firstChild);
-        }//Fin Si
+        borrarError();
         if (!patternTelefono.test(this.value)){
             error.innerHTML ="ERROR: "+this.id.toUpperCase()+" debe contener 9 numeros.";
             this.setAttribute("class","error");
             this.focus();
-            enviar = false;
+            enviarTef = false;
         }else{
             this.removeAttribute("class");
-            enviar = true;
+            enviarTef = true;
         }//Fin Si
     }//Fin Funcion
 
     //Ejercicio 10
     function validarHora() {
-        if (error.innerText.length != 0){
-            error.removeChild(error.firstChild);
-        }//Fin Si
+        borrarError();
         if (!patternHora.test(this.value)){
             error.innerHTML = this.id[0].toUpperCase() + this.id.slice(1)+" errónea. Introdúzcala de nuevo. (hh:mm)";
             this.setAttribute("class","error");           
             this.focus();
-            enviar = false;
+            enviarHora = false;
         }else{
             this.removeAttribute("class");
-            enviar = true;
+            enviarHora = true;
         }//Fin Si
     }//Fin Funcion
 
     //Ejercicio 11
     function validarEnviar(evento){
-        if (!enviar) {
-            if (error.innerText.length != 0){
-                error.removeChild(error.firstChild);
-            }//Fin Si
-            error.innerHTML = "No se puede enviar el formulario con campos invalidos.";
+        if (!enviarNombre || !enviarApellido || !enviarEdad || !enviarNif || !enviarEmail
+            || !enviarProvincia || !enviarFecha || !enviarTef || !enviarHora) {
+            borrarError();
+            error.innerHTML = "No se puede enviar el formulario con campos invalidos/vacios.";
             evento.preventDefault();
         }else{
             var confirmacion = confirm("Enviar formulario");
